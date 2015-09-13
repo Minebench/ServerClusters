240277e50edc3209665b8c2b025a7e12be8a8d98
Max Lee <Phoenix616@users.noreply.github.com>
2015-09-13 17:51:16 +0200

    Add commands on Bukkit side

--- src/main/java/de/themoep/serverclusters/bukkit/ServerClustersBukkit.java
+++ src/main/java/de/themoep/serverclusters/bukkit/ServerClustersBukkit.java
@@ -1,6 +1,14 @@
 package de.themoep.serverclusters.bukkit;
 
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import de.themoep.serverclusters.bukkit.manager.TeleportManager;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
@@ -29,4 +37,18 @@ public class ServerClustersBukkit extends JavaPlugin {
         return tpman;
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(sender instanceof Player) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("RunCommand");
+            out.writeUTF(sender.getName());
+            out.writeUTF(cmd.getName());
+            out.writeUTF(StringUtils.join(args, " "));
+            ((Player) sender).sendPluginMessage(this, "ServerClusters", out.toByteArray());
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED + "This command can only be run by a player!");
+        return true;
+    }
 }
