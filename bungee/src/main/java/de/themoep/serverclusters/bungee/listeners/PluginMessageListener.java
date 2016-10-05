ced02bad2f4a0f6aa925f6966731ded845816f4a
Max Lee <Phoenix616@users.noreply.github.com>
2016-10-05 18:59:55 +0200

    Cleanup and send warning when client tries to send plugin message

--- bungee/src/main/java/de/themoep/serverclusters/bungee/listeners/PluginMessageListener.java
+++ bungee/src/main/java/de/themoep/serverclusters/bungee/listeners/PluginMessageListener.java
@@ -27,68 +27,56 @@ public class PluginMessageListener implements Listener {
 
     @EventHandler
     public void onPluginMessageReceive(PluginMessageEvent event) {
-        if (event.getTag().equals("ServerClusters")) {
-            if (event.getReceiver() instanceof ProxiedPlayer) {
-                ProxiedPlayer receiver = (ProxiedPlayer) event.getReceiver();
-                ByteArrayDataInput in = ByteStreams.newDataInput(event.getData());
-                String subchannel = in.readUTF();
-                if (subchannel.equals("GetPlayerLocation")) {
-                    String reason = in.readUTF();
-                    String sender = in.readUTF();
+        if (!event.getTag().equals("ServerClusters")) {
+            return;
+        }
 
-                    String server = receiver.getServer().getInfo().getName();
-                    String world = in.readUTF();
-                    double x = in.readDouble();
-                    double y = in.readDouble();
-                    double z = in.readDouble();
-                    float yaw = in.readFloat();
-                    float pitch = in.readFloat();
-                    plugin.getLogger().log(Level.INFO, receiver.getName() + " received a plugin message on channel ServerClusters/GetPlayerLocation/" + reason + " from " + sender);
-                    if (reason.equals("Info")) {
-                        // /getpos command or something like that
-                    } else if (reason.equals("SetHome")) {
-                        // set a home, not implemented yet and I'm not even sure if this is the correct way to do it
-                    } else if (reason.equals("SetWarp")) {
-                        // set warp, currently not possible, may get implemented otherwise
-                    } else if (reason.equals("SetSpawn")) {
-                        // look above
-                    }
-                } else if (subchannel.equals("RunCommand")) {
-                    String sender = in.readUTF();
-                    String command = in.readUTF();
-                    LocationInfo loc = null;
+        if (!(event.getReceiver() instanceof ProxiedPlayer)) {
+            // plugin message from the client
+            String senderName = event.getSender().getAddress().toString();
+            if (event.getSender() instanceof ProxiedPlayer) {
+                senderName = ((ProxiedPlayer) event.getSender()).getName();
+            }
+            plugin.getLogger().log(Level.WARNING, senderName + " tried to send plugin message on " + event.getTag() + " channel!");
+            event.setCancelled(true);
+            return;
+        }
 
-                    // Sender is player
-                    if (in.readBoolean()) {
-                        ProxiedPlayer player = plugin.getProxy().getPlayer(sender);
-                        if (player == null) {
-                            plugin.getLogger().log(Level.WARNING, receiver.getName() + " received an invalid plugin message on channel ServerClusters/RunCommand/" + command + " from " + sender + "! Could not find player for that sender?");
-                            ((ProxiedPlayer) event.getReceiver()).sendMessage(ChatColor.RED + "Something went wrong while processing this command! Please contact an admin!");
-                            return;
-                        }
+        ProxiedPlayer receiver = (ProxiedPlayer) event.getReceiver();
+        ByteArrayDataInput in = ByteStreams.newDataInput(event.getData());
+        String subchannel = in.readUTF();
 
-                        loc = new LocationInfo(
-                                player.getServer().getInfo().getName(),
-                                in.readUTF(),
-                                in.readDouble(),
-                                in.readDouble(),
-                                in.readDouble(),
-                                in.readFloat(),
-                                in.readFloat()
-                        );
-                    }
+        if ("RunCommand".equals(subchannel)) {
+            String sender = in.readUTF();
+            String command = in.readUTF();
+            LocationInfo loc = null;
 
-                    String argsStr = in.readUTF();
-                    String[] args = argsStr.split(" ");
-                    plugin.getLogger().log(Level.INFO, receiver.getName() + " received a plugin message on channel ServerClusters/RunCommand/" + command + " '" + argsStr + "' from " + sender + (loc != null ? " at " + loc : ""));
-
-                    if (!plugin.getBukkitCommandExecutor().execute(command, sender, loc, args)) {
-                        plugin.getLogger().log(Level.WARNING, "Error while running ServerClusters/RunCommand/" + command + " from " + sender + "! Command failed to execute?");
-                    }
+            // Sender is player
+            if (in.readBoolean()) {
+                ProxiedPlayer player = plugin.getProxy().getPlayer(sender);
+                if (player == null) {
+                    plugin.getLogger().log(Level.WARNING, receiver.getName() + " received an invalid plugin message on channel ServerClusters/RunCommand/" + command + " from " + sender + "! Could not find player for that sender?");
+                    ((ProxiedPlayer) event.getReceiver()).sendMessage(ChatColor.RED + "Something went wrong while processing this command! Please contact an admin!");
+                    return;
                 }
-            } else {
-                // plugin message from the client
-                event.setCancelled(true);
+
+                loc = new LocationInfo(
+                        player.getServer().getInfo().getName(),
+                        in.readUTF(),
+                        in.readDouble(),
+                        in.readDouble(),
+                        in.readDouble(),
+                        in.readFloat(),
+                        in.readFloat()
+                );
+            }
+
+            String argsStr = in.readUTF();
+            String[] args = argsStr.split(" ");
+            plugin.getLogger().log(Level.INFO, receiver.getName() + " received a plugin message on channel ServerClusters/RunCommand/" + command + " '" + argsStr + "' from " + sender + (loc != null ? " at " + loc : ""));
+
+            if (!plugin.getBukkitCommandExecutor().execute(command, sender, loc, args)) {
+                plugin.getLogger().log(Level.WARNING, "Error while running ServerClusters/RunCommand/" + command + " from " + sender + "! Command failed to execute?");
             }
         }
     }
