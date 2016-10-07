8895ce1af119652f8215d185ff2497ce01bcacf2
Max Lee <Phoenix616@users.noreply.github.com>
2016-10-07 15:55:10 +0200

    Fix NPE on tab completion and add supprot for tabbing second parameter

--- bungee/src/main/java/de/themoep/serverclusters/bungee/commands/TpCommand.java
+++ bungee/src/main/java/de/themoep/serverclusters/bungee/commands/TpCommand.java
@@ -83,18 +83,16 @@ public class TpCommand extends Command implements TabExecutor {
     }
 
     public Iterable<String> onTabComplete(CommandSender sender, String[] strings) {
+        List<String> playerNames = new ArrayList<>();
         if (strings.length == 0) {
-            List<String> playerNames = new ArrayList<String>();
             for (ProxiedPlayer player : plugin.getProxy().getPlayers()) {
                 if (plugin.getVnpbungee() == null || plugin.getVnpbungee().getVanishStatus(player) != VNPBungee.VanishStatus.VANISHED || sender.hasPermission("vanish.see")) {
                     playerNames.add(player.getName());
                 }
             }
             Collections.sort(playerNames);
-            return playerNames;
         } else if (strings.length == 1) {
             String input = strings[0].toLowerCase();
-            List<String> playerNames = new ArrayList<String>();
             for (ProxiedPlayer player : plugin.getProxy().getPlayers()) {
                 if (!player.getName().toLowerCase().startsWith(input)) continue;
                 if (plugin.getVnpbungee() == null || plugin.getVnpbungee().getVanishStatus(player) != VNPBungee.VanishStatus.VANISHED || sender.hasPermission("vanish.see")) {
@@ -102,8 +100,16 @@ public class TpCommand extends Command implements TabExecutor {
                 }
             }
             Collections.sort(playerNames);
-            return playerNames;
+        } else if (strings.length == 2) {
+            String input = strings[1].toLowerCase();
+            for (ProxiedPlayer player : plugin.getProxy().getPlayers()) {
+                if (!player.getName().toLowerCase().startsWith(input)) continue;
+                if (plugin.getVnpbungee() == null || plugin.getVnpbungee().getVanishStatus(player) != VNPBungee.VanishStatus.VANISHED || sender.hasPermission("vanish.see")) {
+                    playerNames.add(player.getName());
+                }
+            }
+            Collections.sort(playerNames);
         }
-        return null;
+        return playerNames;
     }
 }
