ecbaea10da4f344e03acf4e65ac45f3cea330e85
Max Lee <Phoenix616@users.noreply.github.com>
2016-10-12 17:57:06 +0200

    Use correct command name for Delspawn command

--- bungee/src/main/java/de/themoep/serverclusters/bungee/ServerClusters.java
+++ bungee/src/main/java/de/themoep/serverclusters/bungee/ServerClusters.java
@@ -154,7 +154,7 @@ public class ServerClusters extends Plugin {
         commandList.add(new FindCommand(this, "find", "serverclusters.command.find", fal.toArray(new String[fal.size()])));
 
         List<String> dsal = getConfig().getStringList("commandaliases.delspawn");
-        commandList.add(new DelspawnCommand(this, "delwarp", "serverclusters.command.delspawn", dsal.toArray(new String[fal.size()])));
+        commandList.add(new DelspawnCommand(this, "delspawn", "serverclusters.command.delspawn", dsal.toArray(new String[fal.size()])));
 
         List<String> dwal = getConfig().getStringList("commandaliases.delwarp");
         commandList.add(new DelwarpCommand(this, "delwarp", "serverclusters.command.delwarp", dwal.toArray(new String[fal.size()])));
