618a89b179707efb182bd9342936413b69812596
Phoenix616 <mail@moep.tv>
2017-02-15 19:44:57 +0100

    Add explanation, also try to change it first

--- bungee/src/main/java/de/themoep/serverclusters/bungee/listeners/NetworkConnectListener.java
+++ bungee/src/main/java/de/themoep/serverclusters/bungee/listeners/NetworkConnectListener.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 import de.themoep.serverclusters.bungee.events.NetworkConnectEvent;
+import net.md_5.bungee.event.EventPriority;
 
 public class NetworkConnectListener implements Listener {
 
@@ -14,9 +15,12 @@ public class NetworkConnectListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onNetworkConnect(NetworkConnectEvent event) {
-        // Just because why not? No seriously, we need this. Don't ask!
+        // Just because why not? No seriously, we need this!
+        // By setting the target here we make sure that the player connects to his
+        // logout server when his join targets a cluster and not the default server
+        // that BungeeCord wants him to connect to.
         event.setTarget(event.getTarget());
     }
 
