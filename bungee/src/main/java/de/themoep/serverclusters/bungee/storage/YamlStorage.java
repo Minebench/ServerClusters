88f39871b1b304e324928d64b12919f4da3c7f02
Phoenix616 <mail@moep.tv>
2017-02-15 20:08:56 +0100

    Use FIleConfiguration instead of our own yaml provider

--- bungee/src/main/java/de/themoep/serverclusters/bungee/storage/YamlStorage.java
+++ bungee/src/main/java/de/themoep/serverclusters/bungee/storage/YamlStorage.java
@@ -1,36 +1,22 @@
 package de.themoep.serverclusters.bungee.storage;
 
+import de.themoep.bungeeplugin.FileConfiguration;
 import de.themoep.serverclusters.bungee.ServerClusters;
-import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
-import net.md_5.bungee.config.YamlConfiguration;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.logging.Level;
 
 public class YamlStorage extends ValueStorage {
 
-    private Configuration config;
-
-    protected final static ConfigurationProvider ymlCfg = ConfigurationProvider.getProvider(YamlConfiguration.class);
-
-    protected final File configFile;
+    private FileConfiguration config;
 
     public YamlStorage(ServerClusters plugin, String name) {
         super(plugin, name);
-        configFile = new File(plugin.getDataFolder(), name + ".yml");
         try {
-            config = ymlCfg.load(configFile);
-        } catch (FileNotFoundException e) {
-            // Config file not found. Assuming first load.
-            plugin.getLogger().log(Level.INFO, "File '" + configFile.getName() + "' not found! Creating a new one!");
-            config = new Configuration();
-            close();
+            config = new FileConfiguration(plugin, name + ".yml");
         } catch (IOException e) {
-            plugin.getLogger().log(Level.SEVERE, "Error while loading config '" + configFile.getName() + "'!");
+            plugin.getLogger().log(Level.SEVERE, "Error while loading config '" + name + ".yml'!");
             e.printStackTrace();
         }
     }
@@ -50,16 +36,7 @@ public class YamlStorage extends ValueStorage {
      * save configuration to disk
      */
     public void save() {
-        try {
-            ymlCfg.save(config, configFile);
-        } catch (IOException e) {
-            plugin.getLogger().severe("Unable to save configuration at " + configFile.getAbsolutePath());
-            e.printStackTrace();
-        }
-    }
-
-    public Configuration getConfig() {
-        return config;
+        config.saveConfig();
     }
 
     @Override
