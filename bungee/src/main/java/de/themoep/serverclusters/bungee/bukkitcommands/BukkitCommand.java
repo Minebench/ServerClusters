3237176872c95147758c340a412552643dd68290
Phoenix616 <max@themoep.de>
2022-08-07 11:38:02 +0100

    Fix bukkit command execution and use String join

--- bungee/src/main/java/de/themoep/serverclusters/bungee/bukkitcommands/BukkitCommand.java
+++ bungee/src/main/java/de/themoep/serverclusters/bungee/bukkitcommands/BukkitCommand.java
@@ -2,7 +2,6 @@ package de.themoep.serverclusters.bungee.bukkitcommands;
 
 import de.themoep.serverclusters.bungee.LocationInfo;
 import de.themoep.serverclusters.bungee.ServerClusters;
-import de.themoep.serverclusters.bungee.utils.StringUtils;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -25,7 +24,7 @@ public abstract class BukkitCommand extends Command implements TabExecutor {
     @Override
     public void execute(CommandSender sender, String[] args) {
         if (sender instanceof ProxiedPlayer) {
-            plugin.getConnector().getBridge().runServerPlayerCommand((ProxiedPlayer) sender, "/" + getName() + " " + StringUtils.join(args, " "));
+            plugin.getConnector().getBridge().runServerPlayerCommand((ProxiedPlayer) sender, getName() + " " + String.join(" ", args));
         } else {
             sender.sendMessage(ChatColor.RED + "This command can only be run by a player and on the Bukkit server!");
         }
